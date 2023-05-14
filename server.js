cd73cdd01f53140dadb3bcdd149c35b1e4a42692
Rashonda Edwards <rashondaedwards@aol.com>
2023-05-14 11:28:40 -0700

    commit_2

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require("express")
 const app = express()
+require("dotenv").config()
+const PORT = process.env.PORT
 
 app.get("/", (req,res)=>{
     res.send("Hello World")
 })
 
-app.listen(3000)
+app.listen(PORT, ()=>{
+    console.log(`Server is running on port ${PORT}`)
+})
+
+
+module.exports = app
